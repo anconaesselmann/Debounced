f809368306a1f279e0bd84a04934fd134f6badf8
Axel Ancona Esselmann <axel@anconaesselmann.com>
2024-07-11 13:31:11 -0700

    Combine example

---
+++ Examples/DebouncedExample_03/DebouncedExample_03/ContentView.swift
@@ -0,0 +1,54 @@
+//  Created by Axel Ancona Esselmann on 7/11/24.
+//
+
+import SwiftUI
+import Combine
+import Debounced
+
+@MainActor
+class ContentViewModel: ObservableObject {
+
+    @PublishedDebounced(for: 1)
+    var text: String = ""
+
+    var debounced: String = ""
+    var isDebouncing: Bool = false
+    var value: String = ""
+
+    var bag = Set<AnyCancellable>()
+
+    init() {
+        _text.states
+            .dropFirst()
+            .sink { [weak self] in
+                switch $0 {
+                case .debouncing(value: let value, debounced: _):
+                    self?.value = value
+                    self?.isDebouncing = true
+                case .idle(let debounced):
+                    self?.debounced = debounced
+                    self?.isDebouncing = false
+                }
+                self?.objectWillChange.send()
+            }.store(in: &bag)
+    }
+}
+
+struct ContentView: View {
+
+    @StateObject
+    var vm = ContentViewModel()
+
+    var body: some View {
+        VStack {
+            TextField("This text gets debounced", text: $vm.text)
+            Text("Debounced: \"\(vm.debounced)\"")
+            if vm.isDebouncing {
+                Text("Debouncing \"\(vm.text)\"...")
+                ProgressView()
+            }
+            Spacer()
+        }
+        .padding()
+    }
+}
---
+++ Examples/DebouncedExample_03/DebouncedExample_03/DebouncedExample_03App.swift
@@ -0,0 +1,13 @@
+//  Created by Axel Ancona Esselmann on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DebouncedExample_03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
