db0629369f1c97eb5f2205f3de2b16a6edb0ddb8
Axel Ancona Esselmann <axel@anconaesselmann.com>
2024-07-11 12:53:12 -0700

    Initial commit of first example app

---
+++ Examples/DebouncedExample_01/DebouncedExample_01/ContentView.swift
@@ -0,0 +1,28 @@
+//  Created by Axel Ancona Esselmann on 7/11/24.
+//
+
+import SwiftUI
+import Debounced
+
+struct ContentView: View {
+
+    @Debounced(for: 1.0)
+    var text: String = ""
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            TextField("This text gets debounced", text: $text)
+            Text("Debounced: \"\(text)\"")
+            if _text.isDebouncing {
+                Text("Debouncing \"\(_text.value)\"...")
+                ProgressView()
+            }
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Examples/DebouncedExample_01/DebouncedExample_01/DebouncedExample_01App.swift
@@ -0,0 +1,13 @@
+//  Created by Axel Ancona Esselmann on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DebouncedExample_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
